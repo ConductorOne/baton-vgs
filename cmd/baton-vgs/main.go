2420ca22dac590aafbeef70380d566760f15b65f
mchavez <miguel_chavez_m@hotmail.com>
2024-07-26 15:37:28 -0600

    Updating sdk version and viper schema

--- cmd/baton-vgs/main.go
+++ cmd/baton-vgs/main.go
@@ -5,30 +5,42 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/conductorone/baton-sdk/pkg/cli"
+	configSchema "github.com/conductorone/baton-sdk/pkg/config"
 	"github.com/conductorone/baton-sdk/pkg/connectorbuilder"
+	"github.com/conductorone/baton-sdk/pkg/field"
 	"github.com/conductorone/baton-sdk/pkg/types"
-	"github.com/grpc-ecosystem/go-grpc-middleware/logging/zap/ctxzap"
-	"go.uber.org/zap"
-
 	"github.com/conductorone/baton-vgs/pkg/connector"
+	"github.com/grpc-ecosystem/go-grpc-middleware/logging/zap/ctxzap"
+	"github.com/spf13/viper"
+	"go.uber.org/zap"
 )
 
-var version = "dev"
+const (
+	version                    = "dev"
+	connectorName              = "baton-vgs"
+	serviceAccountClientId     = "service-account-client-id"
+	serviceAccountClientSecret = "service-account-client-secret"
+	organizationId             = "organization-id"
+	vault                      = "vault"
+)
+
+var (
+	ServiceAccountClientId     = field.StringField(serviceAccountClientId, field.WithRequired(true), field.WithDescription("The VGS client id."))
+	ServiceAccountClientSecret = field.StringField(serviceAccountClientSecret, field.WithRequired(true), field.WithDescription("The VGS client secret."))
+	OrganizationId             = field.StringField(organizationId, field.WithRequired(true), field.WithDescription("The VGS organization id."))
+	Vault                      = field.StringField(vault, field.WithRequired(true), field.WithDescription("The VGS vault id."))
+	configurationFields        = []field.SchemaField{Vault, ServiceAccountClientId, ServiceAccountClientSecret, OrganizationId}
+)
 
 func main() {
 	ctx := context.Background()
-
-	cfg := &config{}
-	cmd, err := cli.NewCmd(ctx, "baton-vgs", cfg, validateConfig, getConnector)
+	_, cmd, err := configSchema.DefineConfiguration(ctx, connectorName, getConnector, field.NewConfiguration(configurationFields))
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
 		os.Exit(1)
 	}
 
 	cmd.Version = version
-	cmdFlags(cmd)
-
 	err = cmd.Execute()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
@@ -36,13 +48,14 @@ func main() {
 	}
 }
 
-func getConnector(ctx context.Context, cfg *config) (types.ConnectorServer, error) {
+func getConnector(ctx context.Context, cfg *viper.Viper) (types.ConnectorServer, error) {
 	l := ctxzap.Extract(ctx)
 	cb, err := connector.New(ctx,
-		cfg.ServiceAccountClientId,
-		cfg.ServiceAccountClientSecret,
-		cfg.OrganizationId,
-		cfg.Vault)
+		cfg.GetString(serviceAccountClientId),
+		cfg.GetString(serviceAccountClientSecret),
+		cfg.GetString(organizationId),
+		cfg.GetString(vault),
+	)
 	if err != nil {
 		l.Error("error creating connector", zap.Error(err))
 		return nil, err
